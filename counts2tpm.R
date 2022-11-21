5c9a95c602d33a373edf4feb7ffe50d97f86699b
Abderrahman Day <71282936+AbderDay@users.noreply.github.com>
2022-11-21 15:54:21 -0600

    added option to choose Mouse or Human genelist

--- counts2tpm.R
+++ counts2tpm.R
@@ -8,47 +8,57 @@ library(DGEobj.utils)
 library(janitor)
 library(biomaRt)
 
-counts2tpm <- function(subread_counts_path, tpm_path){
+counts2tpm <- function(counts_path, species = "human"){
+  
+  # Get ensemble and gene symbol databases
 
-# Get ensembl gene database to convert gene ensembl ID to gene symbol
-ensembl = useMart("ensembl",dataset="hsapiens_gene_ensembl")
-gene_symbols<-getBM(attributes=c('hgnc_symbol','ensembl_gene_id'), 
-                    mart = ensembl)
+  if (species == "human"){
+    ensembl = useMart("ensembl",dataset="hsapiens_gene_ensembl")
+    attributes = c('hgnc_symbol','ensembl_gene_id')
+    gene_symbols<-getBM(attributes=attributes, mart = ensembl) %>% 
+      dplyr::rename(ensembl_gene_id = 2, gene_symbol = 1)
+  }
+  else if (species == "mouse"){
+    ensembl <- useMart("ensembl", dataset="mmusculus_gene_ensembl")
+    attributes <- c("ensembl_gene_id", "mgi_symbol")
+    gene_symbols<-getBM(attributes=attributes, mart = ensembl) %>% 
+      dplyr::rename(ensembl_gene_id = 1, gene_symbol = 2)
+  }
+  else{
+  }
+  
+  # Get ensembl and gene symbol for MOUSE
+  
+  # Read data from subread_counts.txt (featurecounts output)
+  subread_counts <- read_table(counts_path, skip = 1) 
+  
+  # Extract gene counts matrix
+  counts <- subread_counts %>% 
+    dplyr::select(-c(2:6)) %>% 
+    column_to_rownames(var = "Geneid") %>% 
+    as.matrix()
+  
+  # Extract gene lengths matrix
+  lengths <- subread_counts %>% 
+    dplyr::select(Geneid, length = Length) %>% 
+    column_to_rownames(var = "Geneid") %>% 
+    as.matrix()
+  
+  # Calculate CPM
+  #cpm1 <- convertCounts(counts, "CPM") %>% 
+  #  as.data.frame() %>% 
+  #  rownames_to_column(var = "geneid")
+  
+  # Calculate TPM, add gene symbols, convert empty gene symbols to NA
+  tpm1 <- convertCounts(counts, "TPM", geneLength = lengths) %>% 
+    as.data.frame() %>% 
+    rownames_to_column(var = "ensembl_gene_id") %>% 
+    left_join(gene_symbols, by = "ensembl_gene_id") %>% 
+    relocate(ensembl_gene_id,gene_symbol)
+  
+  tpm1$gene_symbol[tpm1$gene_symbol == ""] <- NA
 
-# Read data from subread_counts.txt (featurecounts output)
-subread_counts <- read_table(subread_counts_path, skip = 1) 
-
-# Extract gene counts matrix
-counts <- subread_counts %>% 
-  dplyr::select(Geneid, t1=PGDX8570T1_RNA, t2=PGDX8570T2_RNA) %>% 
-  column_to_rownames(var = "Geneid") %>% 
-  as.matrix()
-
-# Extract gene lengths matrix
-lengths <- subread_counts %>% 
-  dplyr::select(Geneid, length = Length) %>% 
-  column_to_rownames(var = "Geneid") %>% 
-  as.matrix()
-
-# Calculate CPM
-#cpm1 <- convertCounts(counts, "CPM") %>% 
-#  as.data.frame() %>% 
-#  rownames_to_column(var = "geneid")
-
-# Calculate TPM, add gene symbols, convert empty gene symbols to NA
-tpm1 <- convertCounts(counts, "TPM", geneLength = lengths) %>% 
-  as.data.frame() %>% 
-  rownames_to_column(var = "geneid") %>% 
-  left_join(gene_symbols, by = c("geneid" = "ensembl_gene_id")) %>% 
-  dplyr::select(ensembl_gene_id = geneid,
-                gene_symbol = hgnc_symbol,
-                t1_tpm = t1,
-                t2_tpm = t2)
-tpm1$gene_symbol[tpm1$gene_symbol == ""] <- NA
-
-# Write table
-write_tsv(tpm1, file = tpm_path)
-
+ return(tpm1) 
 }
 
 # Plots
